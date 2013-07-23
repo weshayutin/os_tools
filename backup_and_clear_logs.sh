62b454624beb1146d1a5bf45a0330db92039ff25
wes hayutin <whayutin@redhat.com>
2013-07-23 14:32:51 -0400

    fix backup

--- backup_and_clear_logs.sh
+++ backup_and_clear_logs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-date_time=(date +%F%T)
+date_time=`date +%F%T`
 pushd /var/log/nova
-mkdir date_time
-cp * date_time
+mkdir $date_time
+cp * $date_time
 find . -maxdepth 1 -type f -exec /bin/sh -c "> '{}'" ';'
 
