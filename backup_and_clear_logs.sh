6c7f42de0a155d21107a72177f036bc17a1bc12c
wes hayutin <whayutin@redhat.com>
2013-07-23 14:44:32 -0400

    added other components

--- backup_and_clear_logs.sh
+++ backup_and_clear_logs.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/bash
 
 date_time=`date +%F%T`
-pushd /var/log/nova
-mkdir $date_time
-cp * $date_time
-find . -maxdepth 1 -type f -exec /bin/sh -c "> '{}'" ';'
 
+declare -a components=(nova cinder keystone glance)
+
+for dir in ${components[@]}
+do
+  pushd /var/log/$dir
+  backup_log
+  clear_log
+done
+
+fucntion backup_log {
+  echo "backing up log"
+  mkdir $date_time
+  cp * $date_time
+}
+
+function clear_log {
+  echo "clearing logs"
+  find . -maxdepth 1 -type f -exec /bin/sh -c "> '{}'" ';'
+}
