8fb5192a48050aea171fa81d3dc8b459a7d8fbfa
Wes Hayutin <whayutin@redhat.com>
2013-07-24 12:29:26 -0400

    add havana

--- create_nova_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ssh-keygen -t rsa -f customkey
-chmod 600 customkey*
-nova keypair-add customkey --pub-key customkey.pub
-nova keypair-list
-
--- install_os.sh
+++ install_os.sh
@@ -3,6 +3,9 @@
 if [ $1 == "grizzly" ]; then
   echo "grizzly install"
   yum install -y http://rdo.fedorapeople.org/openstack/openstack-grizzly/rdo-release-grizzly.rpm
+elif [ $1 == "havana" ]; then
+  echo "havana install"
+  yum install -y http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-2.noarch.rpm
 elif [ $1 == "trunk" ]; then
   if [[ `cat /etc/redhat-release` == *Fedora* ]]; then
     wget -O /etc/yum.repos.d/openstack-trunk.repo http://repos.fedorapeople.org/repos/openstack/openstack-trunk/fedora-openstack-trunk.repo
@@ -19,6 +22,8 @@ sed -i 's/PW=.*/PW=dog8code/g' ANSWER_FILE
 sed -i 's/TOKEN=.*/TOKEN=dog8code/g' ANSWER_FILE
 
 #disable quantum/neutron 
-sed -i 's/CONFIG_QUANTUM_INSTALL=y/CONFIG_QUANTUM_INSTALL=n/g' ANSWER_FILE
+if [ $1 == "grizzly" ]; then
+  sed -i 's/CONFIG_QUANTUM_INSTALL=y/CONFIG_QUANTUM_INSTALL=n/g' ANSWER_FILE
+fi
 
 #packstack --answer-file=ANSWER_FILE
