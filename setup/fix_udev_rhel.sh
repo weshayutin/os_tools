6ec2792c2247fff1064e78be6d25885831c64607
wes hayutin <whayutin@redhat.com>
2013-07-31 17:37:22 -0400

    ugh rhel.. painful

--- setup/fix_udev_rhel.sh
+++ setup/fix_udev_rhel.sh
@@ -1,2 +1,10 @@
 mv /etc/udev/rules.d/70-persistent-net.rules /root
-service network restart
+#service network restart
+DEVICE="eth0"
+BOOTPROTO="dhcp"
+NM_CONTROLLED="no"
+ONBOOT="yes"
+TYPE="Ethernet"
+
+cat /dev/null > /etc/udev/rules.d/70-persistent-net.rules
+chattr +i /etc/udev/rules.d/70-persistent-net.rules
