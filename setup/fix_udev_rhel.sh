745cd5d6289a2558a330222ccc2308388e31c791
wes hayutin <whayutin@redhat.com>
2013-08-02 15:29:09 -0400

    add torpedo

--- setup/fix_udev_rhel.sh
+++ setup/fix_udev_rhel.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+cat > /usr/local/removeKernelNetworkingRules.sh << EOL
 mv /etc/udev/rules.d/70-persistent-net.rules /root
-service network restart
+EOL
+chmod +x /usr/local/removeKernelNetworkingRules.sh 
+
+#DEVICE="eth0"
+#BOOTPROTO="dhcp"
+#NM_CONTROLLED="no"
+#ONBOOT="yes"
+#TYPE="Ethernet"
